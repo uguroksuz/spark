fc2998ad1fe797f3f5f5f338d2cd95c6c74f95f5
uguroksuz <uguroksuz@outlook.com>
2019-11-17 14:12:47 +0300

    update test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>SparkSoft</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Yeni Girişim Modelimiz</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
